ae368313e3d76518d7640991bc9b5c0023f244eb
AlbiMusaj <108371047+AlbiMusaj@users.noreply.github.com>
2023-03-11 14:48:11 +0100

    Add files via upload
    Databasat per users contact dhe prona

---
+++ core/Database1.php
@@ -0,0 +1,17 @@
+<?php
+
+class Database1
+{
+    public $pdo;
+
+    public function __construct()
+    {
+        try{
+            $link = new PDO('mysql:host=localhost;dbname=webgr4', 'root', '');
+            $this->pdo = $link;
+        }catch(PDOException $e) {
+            die('DIE'. $e->getMessage());
+        }
+    }
+}
+?>
